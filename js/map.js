9db996633177911a73cc876667385bb57941d906
Vlada Belopolskaya <belopolskayavlada@yandex.ru>
2018-09-25 13:56:18 +0400

    Задание 4.1

--- js/map.js
+++ js/map.js
@@ -11,6 +11,11 @@ var MIN_PRICE = 1000;
 var MAX_PRICE = 1000000;
 var MAX_ROOMS = 5;
 var MAX_GUESTS = 100;
+var PIN_HEIGHT = 70;
+var PIN_WEIGHT = 50;
+var HALF_PIN_WEIGHT = 25;
+var HALF_MAIN_PIN_WEIGHT = 32.5;
+var MAIN_PIN_HEIGHT = 75;
 
 
 /**
@@ -34,12 +39,12 @@ function compareRandom() {
 }
 
 /**
- * Поиск элемента в DOM
+ * Поиск элемента в DOM template
  * @param {string} id айди template
  * @param {string} tag тэг внутри template, который хотим клонировать
  * @return {Element} новый элемент
  */
-function findElement(id, tag) {
+function findElementTemplate(id, tag) {
   return document.querySelector(id).content.querySelector(tag);
 }
 
@@ -52,8 +57,8 @@ function createArrayPins(widthMap) {
   var pins = [];
   for (var i = 0; i < 8; i++) {
     var number = i + 1;
-    var locatoinX = randomInteger(0, widthMap);
-    var locationY = randomInteger(MIN_VERTICAL_COORDINATE, MAX_VERTICAL_COORDINATE);
+    var locatoinX = randomInteger(0, widthMap - PIN_WEIGHT) + HALF_PIN_WEIGHT;
+    var locationY = randomInteger(MIN_VERTICAL_COORDINATE + PIN_HEIGHT, MAX_VERTICAL_COORDINATE - PIN_HEIGHT) + PIN_HEIGHT;
     var arrayFeatures = [];
     var arrayFeatureLenght = randomInteger(0, PIN_FEATURES.length - 1);
     for (var j = 0; j <= arrayFeatureLenght; j++) {
@@ -97,6 +102,7 @@ function createPinElements(pins, templatePin) {
   var fragment = document.createDocumentFragment();
   for (var i = 0; i < 8; i++) {
     var newElement = templatePin.cloneNode(true);
+    newElement.id = 'pin' + i;
     newElement.style.cssText = 'left: ' + pins[i].location.x + 'px; top: ' + pins[i].location.y + 'px;';
     newElement.children[0].src = pins[i].author.avatar;
     newElement.children[0].alt = pins[i].offer.title;
@@ -120,7 +126,7 @@ function addChildtoDom(parentClass, newChild) {
  * @return {Element} новый элемент
  */
 function createСard(arrayElement) {
-  var templateCard = findElement('#card', 'article');
+  var templateCard = findElementTemplate('#card', 'article');
   var newElementCard = templateCard.cloneNode(true);
   var typeOfferEng = arrayElement.offer.type;
   var typeOfferRus = '';
@@ -153,7 +159,6 @@ function createСard(arrayElement) {
   newElementCard.querySelector('.popup__type').textContent = typeOfferRus;
   newElementCard.querySelector('.popup__text--capacity').textContent = arrayElement.offer.rooms + ' комнаты для ' + arrayElement.offer.guests + ' гостей';
   newElementCard.querySelector('.popup__text--time').textContent = 'Заезд после ' + arrayElement.offer.checkin + ' выезд до ' + arrayElement.offer.checkout;
-  newElementCard.querySelector('.popup__features');
   newElementCard.querySelector('.popup__description').textContent = arrayElement.offer.description;
   newElementCard.querySelector('.popup__avatar').src = arrayElement.author.avatar;
 
@@ -182,12 +187,109 @@ function addElementToDomBefore(parentClass, elementBeforeClass, newElement) {
   document.querySelector(parentClass).insertBefore(newElement, document.querySelector(elementBeforeClass));
 }
 
+/**
+ * Поиск всех элементов по классу / id
+ * @param {string} classElements класс элемента
+ * @return {NodeListOf<Element>}
+ */
+
+function findElementAll(classElements) {
+  return document.querySelectorAll(classElements);
+}
+
+/**
+ * Поиск элемента по классу / id
+ * @param {string} classElement класс элемента
+ * @return {Element}
+ */
+function findElement(classElement) {
+  return document.querySelector(classElement);
+}
+
+/**
+ * Блокирует/разблокирует поля fielset внутри класса notice
+ * @param {boolean} argument добавлять ли аргумент disabled fielset
+ */
+function noticeDisabled(argument) {
+  var fieldsetNotice = findElementAll('.notice fieldset');
+  for (var j = 0; j < fieldsetNotice.length; j++) {
+    fieldsetNotice[j].disabled = argument;
+  }
+}
+
+/**
+ * Активирует карту, форму и фильтры, добавляет пины в DOM, навешивает обрабочики на пины
+ */
+function activeState() {
+  var map = findElement('.map');
+  var adForm = findElement('.ad-form');
+  var mapFilters = findElement('.map__filters');
+
+  map.classList.remove('map--faded');
+  adForm.classList.remove('ad-form--disabled');
+  noticeDisabled(false);
+  mapFilters.classList.remove('ad-form--disabled');
+
+  addChildtoDom('.map__pins', pinElements);
+  var newCard = createСard(pins[0]);
+  addElementToDomBefore('.map', '.map__filters-container', newCard);
+
+  var mapPins = findElementAll('.map__pin');
+  for (var i = 0; i < mapPins.length; i++) {
+    mapPins[i].addEventListener('mouseup', showCard);
+  }
+}
+
+/**
+ * Отображает карточку с подробной информацией
+ */
+function showCard() {
+  var element = event.currentTarget;
+  var elementId = element.id;
+  var idIndex = +elementId.substr(3);
+  var newCard = createСard(pins[idIndex]);
+  var oldCard = findElement('.map__card')
+  var parent = findElement('.map');
+
+
+  if (oldCard) {
+    parent.replaceChild(newCard, oldCard);
+  } else {
+    addElementToDomBefore('.map', '.map__filters-container', newCard);
+  }
+
+  var buttonClose = findElement('.popup__close');
+  buttonClose.addEventListener('mouseup', closePopup);
+}
+
+/**
+ * Закрывает попап
+ */
+function closePopup() {
+  var card = findElement('.map__card')
+  var parent = findElement('.map')
+  parent.removeChild(card);
+}
+
+/**
+ * Изменяет адрес , в зависмиости от расположения главного пина
+ */
+function changeAddress() {
+  var address = findElement('#address');
+  var locationX = HALF_MAIN_PIN_WEIGHT + mapPinMain.offsetLeft
+  var locationY = MAIN_PIN_HEIGHT + mapPinMain.offsetTop;
+  address.value = locationX + ', ' + locationY;
+}
+
 var widthMapPins = document.querySelector('.map__pins').offsetWidth; // Ширина окна
 var pins = createArrayPins(widthMapPins);
-var templatePin = findElement('#pin', 'button');
+var templatePin = findElementTemplate('#pin', 'button');
 var pinElements = createPinElements(pins, templatePin);
-addChildtoDom('.map__pins', pinElements);
-var newCard = createСard(pins[0]);
-addElementToDomBefore('.map', '.map__filters-container', newCard);
 
+noticeDisabled(true);
+var mapPinMain = findElement('.map__pin--main');
+var address = findElement('#address');
+address.value = mapPinMain.offsetLeft + ', ' + mapPinMain.offsetTop;
+mapPinMain.addEventListener('mouseup', activeState);
+mapPinMain.addEventListener('mouseup', changeAddress);
 
